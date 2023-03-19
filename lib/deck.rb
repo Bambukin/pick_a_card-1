1c3ac713daf60f680d8fe151a2803434528e8263
Vadim <vadimsorella@gmail.com>
2023-03-19 16:06:31 +0300

    Deck & Card classes

---
+++ lib/deck.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+require_relative 'card'
+class Deck
+  def initialize
+    @cards = []
+
+    Card::VALUES.each do |value|
+      Card::SUITS.each do |suit|
+        @cards << Card.new(value, suit)
+      end
+    end
+
+    2.times do
+      @cards << Card.new(nil, nil, true )
+    end
+  end
+
+  def shuffle
+    @cards.shuffle!
+
+    self
+  end
+
+  def get_a_card
+    @cards.sample
+  end
+
+  def pop_a_card
+    @cards.pop
+  end
+end
--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,25 +1,15 @@
 # encoding: utf-8
+require_relative 'lib/deck'
+
 puts 'Pick a Card.'
+puts
 
-values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+deck = Deck.new.shuffle
 
-cards = []
-
-values.each do |value|
-  suits.each do |suit|
-    cards << "#{value}#{suit[0].downcase}"
-  end
-end
-
-2.times do
-  cards << '[Joker]'
-end
-
-cards.shuffle!
 puts 'How many cards do you pick?'
 number = $stdin.gets.to_i
 
+puts
 number.times do
-  puts cards.pop
+  puts deck.pop_a_card
 end
